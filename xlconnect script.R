ea9f78d5a69778c86019c65762462a8bca009fa7
Tim Hill <tim.hill.nz@gmail.com>
2017-02-03 13:27:00 +1300

    Finished first go at function for tidying data

--- xlconnect script.R
+++ xlconnect script.R
@@ -1,8 +1,9 @@
 library(XLConnect)
-library(dplyr)
+library(tidyr)
 
 
-ws <- loadWorkbook("Top-baby-names-1954-2015.xlsx")
+download.file("https://smartstart.services.govt.nz/assets/files/Top-baby-names-1954-2016.xlsx", "data/names.xlsx", mode = "wb")
+ws <- loadWorkbook("data/names.xlsx")
 names.ws <- getSheets(ws)
 
 #read.names <- function(x){
@@ -10,43 +11,45 @@ names.ws <- getSheets(ws)
 #}
 
 
-girlsdata <- readWorksheet(ws, "Girls' Names", startRow = 4, endRow = 107)
-boysdata <- readWorksheet(ws, "Boys' Names", startRow = 4, endRow = 107)
+girlsdata <- readWorksheet(ws, "Girls' Names", startRow = 4, startCol = 2)
+boysdata <- readWorksheet(ws, "Boys' Names", startRow = 4, startCol = 2)
 
 
 ## The excel spreadsheet is a mess for data analysis - the following works to clean 
-## and tidy the data but is iterative and not the simplest script. Something to 
+## and tidy the data but is iterative and not the simplest script; something to 
 ## revisit in the future.
 
 
 ### Tidy girls names data ======================================
-girlsdata[1,95] <- girlsdata[1,96]
 
-girlsdata <- tbl_df(girlsdata)
-girlsdata <- girlsdata[-3,-2]
-girlsdata[1,1] <- 1954
+#remove rows that start with NA
 
-for (h in seq(1,186, by=3)){
-  yearvec <- as.numeric(rep(girlsdata[1,h],100))
-  girlsdata[3:102,h] <- yearvec
-  girlsdata[2,h] <- "Year"
-  girlsdata[2,h+1] <- "Name"
-  girlsdata[2,h+2] <- "No."
+#add the year to every third column
+year.vec <- girlsdata[1,grep("[0-9]{4}",girlsdata[1,])]   #get a vector of the years
+num.yrs <- length(year.vec)   #find out how many years are included
+yr.seq <- seq(1, ncol(girlsdata), by = 3)
+
+girlsdata <- girlsdata[!is.na(girlsdata[,1]),]
+
+#remove extraneous columns & rows
+col.seq <- seq(1, ncol(girlsdata), by = 3)   #create an index of the columns (every third one) that are to contain the year
+#girlsdata <- girlsdata[-c(1:3),-col.seq]   #remove extraneous rows/columns
+
+for(i in 1:length(year.vec)) {   #loop through the column index and add the years
+  girlsdata[,yr.seq[i]] <- year.vec[i]
 }
-rm(h)
 
-colnames(girlsdata) <- girlsdata[2,]
+#add column names
+names(girlsdata) <- rep(c("year","name","number"), times = num.yrs)
 
-girlsdata <- girlsdata[-c(1,2),]
+#tidy the data
 
-girlsdata2 <- data.frame(Year = as.numeric(),
-                         Name = as.character(),
-                         No. = as.numeric())
+stack(girlsdata)
 
-for (i in seq(1,186, by=3)){
-  girlsdata2 <- rbind(girlsdata2,girlsdata[,i:(i+2)])
-}
-girlsdata2$Sex <- rep("Girl", times = length(girlsdata2$Year))
+
+girlsdata2 <- gather(girlsdata, "year", "name", "number", 1:ncol(girlsdata))
+
+
 
 
 ### Tidy Boys data ==========================================
@@ -93,6 +96,19 @@ namedata <- namedata %>% group_by(Sex, Year) %>%
 namedata[3500,2] <- "Micheal"
 
 write.csv(namedata, row.names = F, file = "NZnamedata.csv")
+
+
+### Also process birth data from statistics.govt.nz
+
+birthdata <- read_csv("~/Dropbox/R_files/names/Birth_data.csv",
+                col_names = c("year","liveMaleMaori","liveMaleTotal","liveFemaleMaori","liveFemaleTotal","liveMaori",
+                "liveTotal","stillbirthMaori","stillbirthTotal","totalBirthsMaori","totalBirths"),
+                col_types = "iiiiiiiiiii",
+                na = c("..","NA"),  skip = 3, n_max = 81)
+
+birthdata <- birthdata %>% gather(type, number, -year)
+
+
 
 
 
